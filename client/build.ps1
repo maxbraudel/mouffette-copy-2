cfda49b84deac03c6cd2492fbdcd992f2013d36a
Max Braudel <dev.maxbraudel@gmail.com>
2025-10-25 21:32:06 +0200

    fix

--- client/build.ps1
+++ client/build.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Building Mouffette Client (Windows)" -ForegroundColor Cyan
 
-Remove-Item -Path (Join-Path $PSScriptRoot "client\build") -Recurse -Force
+Remove-Item -Path (Join-Path $PSScriptRoot "build") -Recurse -Force
 
 # 1) Ensure MSYS2 UCRT64 toolchain (Qt6) is available
 $msysRoot = 'C:\msys64'
